fe1f3cf12359b01e9fd051bc7c8270ec1ea6d161
rumkin <dev@rumk.in>
2013-10-05 23:37:28 +0400

    Interface changed

---
+++ test/example.js
@@ -0,0 +1,32 @@
+conclude.after('all-tasks', function(){
+	console.log('All tasks complete');
+});
+
+// Define tasks with a string
+conclude.after('mysql mongo memcache', function(){
+	console.log('Ready A');
+});
+// Define tasks as an Array
+conclude.after(['mysql', 'mongo', 'memcache'], function(){
+	console.log('Ready B');
+});
+// Define tasks as arguments
+conclude.after('mysql', 'mongo', 'memcache', function(){
+	console.log('Ready C');
+});
+// All styles together
+conclude.after(['mysql'], 'mongo memcache', function(){
+	console.log('Ready D');
+});
+
+var complete = conclude.getComplete();
+
+process.nextTick(function(){
+	// All-tasks will be ready after others
+	conclude.task('other').after('memcached mongo mysql');
+	// Notify conclude
+	conclude.complete('mysql');
+	// Notify with closure
+	complete('mongo');
+	complete('memcache');
+});
